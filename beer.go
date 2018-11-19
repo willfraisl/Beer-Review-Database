ceb6985fb9cebdfb19fa39b6eb8604874f8f74e6
willfraisl <willfraisl@gmail.com>
2018-11-19 14:24:53 -0800

    skeleton for database connection

---
+++ beer.go
@@ -0,0 +1,42 @@
+/* File Name: beer.go
+ * Authors: Will Fraisl and Max McKee
+ * Description:
+ * Usage: go build beer.go
+ * ./beer
+ */
+
+package main
+
+import (
+    "fmt"
+    "database/sql"
+    _ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+    fmt.Println("Connecting to Database...")
+    db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/beer_database")
+    
+    // if there is an error opening the connection, handle it
+    if err != nil {
+        panic(err.Error())
+    }
+    
+    // defer the close till after the main function has finished
+    // executing 
+	defer db.Close()
+	
+	create_tables(db)
+}
+
+func init_database(db *sql.DB) {
+	// perform a db.Query create 
+    create, err := db.Query("CREATE TABLE test_table(test_attribute INT)")
+    
+    // if there is an error inserting, handle it
+    if err != nil {
+        panic(err.Error())
+    }
+    // be careful deferring Queries if you are using transactions
+    defer create.Close()
+}
