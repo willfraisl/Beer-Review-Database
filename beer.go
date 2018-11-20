272aa87cb9af2f1dd6215410224d294a37adc5b2
willfraisl <willfraisl@gmail.com>
2018-11-19 16:13:30 -0800

    initial sql script

--- beer.go
+++ beer.go
@@ -15,7 +15,9 @@ import (
 
 func main() {
 	fmt.Println("Hello and welcome to the Beer Review Database!!")
-	fmt.Println("Please enter your username: ")
+	fmt.Println("Are you a brewer, vendor, or rater?")
+	fmt.Println("Enter username: ")
+	fmt.Println("Enter password: ")
 
     db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/beer_database")
     
--- init_database.sql
+++ init_database.sql
@@ -1,10 +1,40 @@
-DROP TABLE IF EXISTS test_table;
-DROP TABLE IF EXISTS test_table2;
+DROP TABLE IF EXISTS brewery;
+DROP TABLE IF EXISTS rater;
+DROP TABLE IF EXISTS vendor;
+DROP TABLE IF EXISTS beer;
+DROP TABLE IF EXISTS rating;
 
-CREATE TABLE test_table(
-    test_attribute INT
+
+CREATE TABLE brewery(
+    name VARCHAR(50),
+    password VARCHAR(50),
+    location VARCHAR(50),
+    PRIMARY KEY (name)
 );
 
-CREATE TABLE test_table2(
-    test_attribute2 INT
+CREATE TABLE rater(
+    username VARCHAR(50),
+    password VARCHAR(50),
+    PRIMARY KEY (username)
+);
+
+CREATE TABLE vendor(
+    name VARCHAR(50),
+    password VARCHAR(50),
+    location VARCHAR(50),
+    PRIMARY KEY (name)
+);
+
+CREATE TABLE beer(
+    name VARCHAR(50),
+    abv DECIMAL(4,3),
+    ibu INT,
+    PRIMARY KEY (name)
+);
+
+CREATE TABLE rating(
+    id INT,
+    stars INT,
+    description VARCHAR(150),
+    date DATE
 );
